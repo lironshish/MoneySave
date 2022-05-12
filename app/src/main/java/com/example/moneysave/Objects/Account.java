d438594eabd47d6f2d5df8e71d9d17b943a748e6
lironshish <lironshish1@gmail.con>
2022-05-12 11:51:43 +0300

    Setters and Getters

--- app/src/main/java/com/example/moneysave/Objects/Account.java
+++ app/src/main/java/com/example/moneysave/Objects/Account.java
@@ -5,4 +5,26 @@ import java.util.ArrayList;
 public class Account {
     private String name;
     private ArrayList<User> SharedWith = new ArrayList<User>();
+
+    public Account(){ }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<User> getSharedWith() {
+        return SharedWith;
+    }
+
+    public void setSharedWith(ArrayList<User> sharedWith) {
+        SharedWith = sharedWith;
+    }
+
+    //Need to add functions that pull and push data to the database
+
 }
