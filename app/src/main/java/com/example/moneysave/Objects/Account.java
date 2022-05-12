b5473f0ebcb093a259ae18b7eef9213fe185ff7e
lironshish <lironshish1@gmail.con>
2022-05-12 11:46:35 +0300

    Add Objects

--- app/src/main/java/com/example/moneysave/Objects/Account.java
+++ app/src/main/java/com/example/moneysave/Objects/Account.java
@@ -1,4 +1,8 @@
 package com.example.moneysave.Objects;
 
+import java.util.ArrayList;
+
 public class Account {
+    private String name;
+    private ArrayList<User> SharedWith = new ArrayList<User>();
 }
---
+++ app/src/main/java/com/example/moneysave/Objects/User.java
@@ -0,0 +1,17 @@
+package com.example.moneysave.Objects;
+
+import android.provider.ContactsContract;
+
+public class User {
+    private String name;
+    private ContactsContract.CommonDataKinds.Email email;
+    private String password;
+    // need to add avatar
+
+
+    public User(){ }
+
+    public User(String name, ContactsContract.CommonDataKinds.Email email, String password){
+
+    }
+}
