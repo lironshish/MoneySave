80bd9f9b5547c41073567db04f9404120ac926b7
lironshish <lironshish1@gmail.con>
2022-05-12 15:39:20 +0300

    Add popup window to insert account name

--- app/src/main/java/com/example/moneysave/Acivities/MyAccountsActivity.java
+++ app/src/main/java/com/example/moneysave/Acivities/MyAccountsActivity.java
@@ -3,13 +3,71 @@ package com.example.moneysave.Acivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.Toast;
+
 import com.example.moneysave.R;
 
 public class MyAccountsActivity extends AppCompatActivity {
+    com.google.android.material.floatingactionbutton.FloatingActionButton addAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_accounts);
+        InitButtons();
+
+
+
+    }
+
+    public void InitButtons(){
+        addAccount = findViewById(R.id.account_BTN_Add);
+
+        addAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                onButtonShowPopupWindowClick(v);
+                Log.d("liron","clicked");
+                onButtonShowPopupWindowClick(v);
+            }
+        });
+    }
+
+
+
+    public void onButtonShowPopupWindowClick(View view) {
+        Log.d("shay","intoPopUp");
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.add_account_popup_window, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 }
