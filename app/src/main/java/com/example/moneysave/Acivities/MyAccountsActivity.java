3e0e5f7cdd783eaaa67fb077dfd7111e4af8af49
lironshish <lironshish1@gmail.con>
2022-05-12 15:21:30 +0300

    Add My accounts activity

---
+++ app/src/main/java/com/example/moneysave/Acivities/MyAccountsActivity.java
@@ -0,0 +1,15 @@
+package com.example.moneysave.Acivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.moneysave.R;
+
+public class MyAccountsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_accounts);
+    }
+}
