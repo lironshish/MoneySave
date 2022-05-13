4ef313633a134e19a9bbf2f9c6781955aba17029
yuvalitach <yuvalitach1@gmail.com>
2022-05-13 18:26:13 +0300

    add pie chart

--- app/src/main/java/com/example/moneysave/Acivities/temp.java
+++ app/src/main/java/com/example/moneysave/Acivities/temp.java
@@ -11,22 +11,28 @@ import com.example.moneysave.R;
 import org.eazegraph.lib.charts.PieChart;
 import org.eazegraph.lib.models.PieModel;
 
-public class PieChartActivity extends AppCompatActivity {
-    // Create the object of TextView and PieChart class
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
+
+public class temp extends AppCompatActivity {
     TextView tvR, tvPython, tvCPP, tvJava;
     PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pie_chart);
+        setContentView(R.layout.temp);
 
         // Link those objects with their respective
-    // id's that we have given in .XML file
-        tvR = findViewById(R.id.tvR);
-        tvPython = findViewById(R.id.tvPython);
-        tvCPP = findViewById(R.id.tvCPP);
-        tvJava = findViewById(R.id.tvJava);
+        // id's that we have given in .XML file
+//        tvR = findViewById(R.id.tvR);
+//        tvPython = findViewById(R.id.tvPython);
+//        tvCPP = findViewById(R.id.tvCPP);
+//        tvJava = findViewById(R.id.tvJava);
         pieChart = findViewById(R.id.piechart);
 
         // Creating a method setData()
@@ -36,37 +42,35 @@ public class PieChartActivity extends AppCompatActivity {
 
     private void setData(){
         // Set the percentage of language used
-        tvR.setText(Integer.toString(40));
-        tvPython.setText(Integer.toString(30));
-        tvCPP.setText(Integer.toString(5));
-        tvJava.setText(Integer.toString(25));
+//        tvR.setText(Integer.toString(40));
+//        tvPython.setText(Integer.toString(30));
+//        tvCPP.setText(Integer.toString(5));
+//        tvJava.setText(Integer.toString(25));
 
         // Set the data and color to the pie chart
         pieChart.addPieSlice(
                 new PieModel(
                         "R",
-                        Integer.parseInt(tvR.getText().toString()),
+                        20,
                         Color.parseColor("#FFA726")));
         pieChart.addPieSlice(
                 new PieModel(
                         "Python",
-                        Integer.parseInt(tvPython.getText().toString()),
+                        20,
                         Color.parseColor("#66BB6A")));
         pieChart.addPieSlice(
                 new PieModel(
                         "C++",
-                        Integer.parseInt(tvCPP.getText().toString()),
+                        30,
                         Color.parseColor("#EF5350")));
         pieChart.addPieSlice(
                 new PieModel(
                         "Java",
-                        Integer.parseInt(tvJava.getText().toString()),
+                        30,
                         Color.parseColor("#29B6F6")));
 
         // To animate the pie chart
         pieChart.startAnimation();
 
     }
-
-
 }
