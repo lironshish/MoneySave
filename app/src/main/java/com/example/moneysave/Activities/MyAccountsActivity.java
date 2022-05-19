18c9fa72b3272cf8c81c9641a9d978a869b83d56
Lilach Laniado <lilush95@gmail.con>
2022-05-19 11:57:29 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/moneysave/Acivities/AddAccount_Activity.java
    #	app/src/main/java/com/example/moneysave/Activities/DetailsActivity.java
    #	app/src/main/res/layout/account_activity.xml
    #	app/src/main/res/layout/activity_add_account.xml

--- app/src/main/java/com/example/moneysave/Activities/MyAccountsActivity.java
+++ app/src/main/java/com/example/moneysave/Activities/MyAccountsActivity.java
@@ -1,12 +1,16 @@
 package com.example.moneysave.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.moneysave.Adapter.Account_Adapter;
+import com.example.moneysave.Data;
 import com.example.moneysave.Objects.Account;
+import com.example.moneysave.Objects.Goal;
 import com.example.moneysave.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
@@ -15,7 +19,7 @@ public class MyAccountsActivity extends AppCompatActivity {
 
     private FloatingActionButton addAccount;
     private RecyclerView account_list;
-    ArrayList<Account> accounts = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,38 +27,42 @@ public class MyAccountsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_accounts);
 
         InitButtons();
+        initAdapter();
 
-//        account_list = findViewById(R.id.accounts_list);
-//
-//
-//        AccountAdapter accountAdapter = new AccountAdapter(this, accounts);
-//        account_list.setLayoutManager(new LinearLayoutManager(this));
-//        account_list.setHasFixedSize(true);
-//        account_list.setAdapter(accountAdapter);
-//
-//        accountAdapter.setAccountlistener(new AccountAdapter.Accountlistener() {
-//
-//            @Override
-//            public void sharedWith(Account account, int position) {
-//                //TODO
-//            }
-//
-//            @Override
-//            public void minus(Account account, int position) {
-//                //TODO
-//            }
-//        });
+
 
     }
 
 
     public void InitButtons(){
+        account_list = findViewById(R.id.myAccounts_LST_accounts);
         addAccount = findViewById(R.id.account_BTN_Add);
 
         addAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 replaceActivity();
+            }
+        });
+    }
+
+    private void initAdapter() {
+        ArrayList<Account> accounts = Data.generateAccounts();
+        Account_Adapter accountAdapter = new Account_Adapter(this, accounts);
+        account_list.setLayoutManager(new LinearLayoutManager(this));
+        account_list.setHasFixedSize(true);
+        account_list.setAdapter(accountAdapter);
+
+        accountAdapter.setAccountlistener(new Account_Adapter.Accountlistener() {
+
+            @Override
+            public void sharedWith(Account account, int position) {
+                //TODO
+            }
+
+            @Override
+            public void minus(Account account, int position) {
+                //TODO
             }
         });
     }
